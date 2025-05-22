54edbc9ecdd40b99fa10bf62302490054e53ee04
shreyas-vivek <shreyasvivek01@gmail.com>
2025-05-23 00:24:04 +1000

    final commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import SpotThePhish from './SpotThePhish';
-import './index.css';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename="/Landing-page.github">
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/phish" element={<SpotThePhish />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
